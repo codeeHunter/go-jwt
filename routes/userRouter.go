ded0b8975f76191514ca13beb372133515db6887
codeeHunter <chishok32@gmail.com>
2023-08-22 20:21:18 +0400

    Add project in git

---
+++ routes/userRouter.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	controller "github.com/codeeHunter/golang-jwt/controllers"
+	"github.com/codeeHunter/golang-jwt/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/user/:id", controller.GetUsers())
+}
